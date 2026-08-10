84e011743df122a9c313a4702ad0be4c12df17e8
ashaherath152-stack <ashaherath152@gmail.com>
2026-08-10 14:21:52 +0530

    Connect app to Aiven MySQL

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: 'railway',
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST || process.env.MYSQLHOST || 'localhost',
+  port: Number(process.env.DB_PORT || process.env.MYSQLPORT || 3306),
+  user: process.env.DB_USER || process.env.MYSQLUSER || 'root',
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || '',
+  database:
+    process.env.DB_NAME ||
+    process.env.MYSQL_DATABASE ||
+    'charifashion',
+
+  ssl: process.env.DB_CA_CERT
+    ? {
+        ca: process.env.DB_CA_CERT.replace(/\\n/g, '\n'),
+        rejectUnauthorized: true
+      }
+    : undefined,
+
   waitForConnections: true,
   connectionLimit: 10
 });
